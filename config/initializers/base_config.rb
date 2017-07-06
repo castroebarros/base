3e1a238949825dff21f3300a10182e29b7e9709d
Lucas Castro <castro.lucas@gmail.com>
2017-07-05 21:16:11 -0300

    Removing duplicated configuration (present on application.rb)

--- config/initializers/base_config.rb
+++ config/initializers/base_config.rb
@@ -1,10 +1,6 @@
 config = Rails.application.config
 config.time_zone = 'Brasilia'
 config.i18n.default_locale = 'pt-BR'
-config.generators do |g|
-  g.stylesheets false
-  g.javascripts false
-end
 
 if Rails.env.production? 
   # DO fallback to assets pipeline if a precompiled asset is missed.
