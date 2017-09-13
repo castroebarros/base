2f4410ddcd42c045f1407d734d81b6f2612f8c61
Lucas Castro <castro.lucas@gmail.com>
2017-09-13 15:54:16 -0300

    Putting the formatters inside in a function in order they work on nested form event

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -23,14 +23,8 @@
 //= require_tree .
 
 jQuery(function() {
-  $('.money').maskMoney({prefix: "", allowZero: true, allowNegative: false, thousands: ".", decimal: ",", affixesStay: false});
-  $('.select2').select2({language: 'pt-BR'});
 
-  $('.cpf').mask('000.000.000-00');
-  $('.cep').mask('00.000-000');
-  $('.cnpj').mask('00.000.000/0000-00');
-
-  /* Configuração de máscara para 9 dígitos. */
+  /* Configuring phone mask to allow 8 or 9 digits. */
   var SPMaskBehavior = function (val) {
     return val.replace(/\D/g, '').length === 11 ? '(00) 00000-0000' : '(00) 0000-00009';
   },
@@ -39,5 +33,18 @@ jQuery(function() {
       field.mask(SPMaskBehavior.apply({}, arguments), options);
     }
   };
-  $('.phone').mask(SPMaskBehavior, spOptions);
+
+  // Wrapping the formatters in order they can be used inside nested forms.
+  formatters = function()  {
+    $('.money').maskMoney({prefix: "", allowZero: true, allowNegative: false, thousands: ".", decimal: ",", affixesStay: false});
+    $('.select2').select2({language: 'pt-BR'});
+
+    $('.cpf').mask('000.000.000-00');
+    $('.cep').mask('00.000-000');
+    $('.cnpj').mask('00.000.000/0000-00');
+    $('.phone').mask(SPMaskBehavior, spOptions);
+  }
+  formatters();
+  $(document).on('nested:fieldAdded', formatters);
+
 });
